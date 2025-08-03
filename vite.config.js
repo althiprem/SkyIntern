2f0c52f0373f451fe2bae254271d4203c85ef09f
Althi prem kumar <142485161+althiprem@users.noreply.github.com>
2025-08-03 22:41:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
 });
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-});
->>>>>>> 3f1769fa4a559e23189bb7197d9dff51380dae01
